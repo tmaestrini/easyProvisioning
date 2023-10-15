4759f4a1bc5f640d973ddd97e2803c3b55b3e3a5
Tobias Maestrini <tobias@bee365.ch>
2023-10-15 16:00:04 +0200

    fix references

--- src/Private/Connect-SPOAdminUrl.ps1
+++ src/Private/Connect-SPOAdminUrl.ps1
@@ -7,7 +7,7 @@ Function Connect-SPOAdminUrl {
   
   Write-Host "Please log into SPO Admin Center with credentials <$($SPOTemplateConfig.AdminUPN)>. Press <ENTER> to proceed..." -NoNewline
   $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
-  $global:SPOAdminConnection = Connect-PnPOnline -Url $SPOTemplateConfig.AdminUrl -Interactive -ReturnConnection
-  if ($null -eq $global:AdminConnection) { throw "❌ failed!" }
+  $global:SPOAdminConnection = Connect-PnPOnline -Url $Url -Interactive -ReturnConnection
+  if ($null -eq $global:SPOAdminConnection) { throw "❌ failed!" }
   Write-Host " ✔︎ OK" -ForegroundColor DarkGreen
 }
