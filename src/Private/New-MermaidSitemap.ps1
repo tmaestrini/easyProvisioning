ddcb7d52953899ffa034e5f6d6eceda5e77c7a64
Tobias Maestrini <tobias@bee365.ch>
2023-12-05 21:25:20 +0100

    add factory to create sitemap

--- src/Private/New-MermaidSitemap.ps1
+++ src/Private/New-MermaidSitemap.ps1
@@ -17,8 +17,9 @@ function New-MermaidSitemap {
   $sitemapHubsites = @()
 
   foreach ($siteStructure in $template.SharePoint.Structure) {
-    $node = $siteStructure.values.Type -eq 'Communication' ? "$($siteStructure.values.Url)(""%content%""):::CommSite" : $siteStructure.values.Type -eq 'Team' ? "$($siteStructure.values.Url)[""%content%""]:::TeamSite" : "$($siteStructure.values.Url)[""%content%""]:::SPOTeamSite"
-    # $node = $siteStructure.values.IsHub -eq $true ? $node.Replace('%content%', 'fa:fa-home %content%') : $node
+    $node = $siteStructure.values.Type -eq 'Communication' ? "$($siteStructure.values.Url)(""%content%""):::CommSite" 
+      : $siteStructure.values.Type -eq 'Team' ? "$($siteStructure.values.Url)[""%content%""]:::TeamSite" 
+      : "$($siteStructure.values.Url)[""%content%""]:::SPOTeamSite"
     $node = $siteStructure.values.IsHub -eq $true ? $node.Replace('%content%', '☆ %content%') : $node
     $node = $node.Replace('%content%', "<strong>$($siteStructure.keys)</strong><br/><hr/><small>▷$($siteStructure.values.Url)</small><div style='font-size: 0.8em;display:flex;flex-direction:column;line-height:1em'>%content%</div>")
     
@@ -27,13 +28,12 @@ function New-MermaidSitemap {
       $node = $node.Replace('%content%', "<div style='position: relative;left: 2px;border-left: 0.5px solid black;padding: 0.5em 0 0 0;'>– $($siteContent.values.Title) <br/>   <span style='font-size:0.8em'>$($siteContent.keys)</span> </div>%content%")    
     }
     
-    $node = $node.Replace('%content%', "")    
+    $node = $node.Replace('%content%', "") 
     
     # node is ready – add it to site map
     $sitemapContent += "$node`n"
     
     if ($siteStructure.values.ConnectedHubsite) {
-      # $sitemapContent += "$($siteStructure.values.ConnectedHubsite)[[fa:fa-home $($siteStructure.values.ConnectedHubsite)]]`n"
       $sitemapRelations += "$($siteStructure.values.ConnectedHubsite) --> $($siteStructure.values.Url)`n"
     }
   }
@@ -45,7 +45,7 @@ function New-MermaidSitemap {
   }
 
   $sitemapDirectory = Join-Path -Path $PSScriptRoot -ChildPath ../../templates
-  $sitemapPath = Join-Path -Path $sitemapDirectory -ChildPath "$($TemplateName).sitemap"
+  $sitemapPath = Join-Path -Path $sitemapDirectory -ChildPath "$($TemplateName -replace '(^.*)(\.yml)', '$1.sitemap.mmd')"
 
   $sitemapTemplatePath = Join-Path -Path $PSScriptRoot -ChildPath ../../artifacts/sitemapTemplate.eps -Resolve
   $sitemapConfig = Invoke-EpsTemplate -Path $sitemapTemplatePath -Binding $Binding
---
+++ src/Public/New-SiteMap.ps1
@@ -0,0 +1,19 @@
+function New-Sitemap {
+
+  [cmdletbinding()]
+  param(
+    [Parameter( Mandatory = $true, HelpMessage = "Full name of the template, including .yml (aka <name>.yml)")]
+    [string]$TemplateName,
+    [Parameter(Mandatory = $true)]
+    [ValidateSet('Mermaid')] $Type
+  )
+
+  $template = Get-Template -TemplateName $TemplateName
+  if ($null -eq $template.SharePoint.TenantId) { throw "No SharePoint tenant id provided" }
+  if ($null -eq $template.SharePoint.Structure) { throw "No SharePoint Structure provided" }
+
+  switch ($Type) {
+    'Mermaid' { New-MermaidSitemap -TemplateName $TemplateName }
+    Default {}
+  }
+}
