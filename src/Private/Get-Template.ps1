3fe06a5322bcab3bbc467b464e3abbe31ece4419
Tobias Maestrini <tobias@bee365.ch>
2024-04-25 17:16:35 +0200

    add merge function for contained templates

--- src/Private/Get-Template.ps1
+++ src/Private/Get-Template.ps1
@@ -2,11 +2,27 @@ Function Get-Template {
   [cmdletbinding()]
   param(
     [Parameter(
-        Mandatory = $true,
-        HelpMessage = "Full name of the template, including .yml (aka <name>.yml)"
+      Mandatory = $true,
+      HelpMessage = "Full name of the template, including .yml (aka <name>.yml)"
     )][string]$TemplateName
   )
 
+  # Merge contained templates into main config
+  function Merge-ContainedTemplates {
+    if ($Config.Contains) {
+      $Content = (Get-Content -Path $Config.Contains) -join "`n"
+      $ContainedConfig = ConvertFrom-Yaml $Content
+  
+      # SharePoint
+      if ($Config.SharePoint.TenantId -eq $ContainedConfig.SharePoint.TenantId) {
+        foreach ($element in $ContainedConfig.SharePoint.Structure) {
+          $Config.SharePoint.Structure.Add($element)
+        }
+      }
+    }
+  }
+
+  # Starting point
   $ConfigPath = Join-Path -Path $PSScriptRoot -ChildPath ../../tenants/$TemplateName
   if (-not(Test-Path -Path $ConfigPath -PathType Leaf)) {
     Write-Error "No template found for '$Tenant'" -ErrorAction Stop
@@ -14,5 +30,7 @@ Function Get-Template {
 
   $Content = (Get-Content -Path $ConfigPath) -join "`n"
   $Config = ConvertFrom-Yaml $Content
+  
+  Merge-ContainedTemplates
   return $Config
 }
